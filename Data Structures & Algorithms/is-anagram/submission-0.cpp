e2863745e601f9a748f23ad67495c64dd5da9a39
Arsh Iqbal <144552708+arsh9958@users.noreply.github.com>
2026-07-09 09:42:25 +0530

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        
+        if(n==m && s==t){
+            return true;
+        }
+        return false;
+
+         
+    }
+};
